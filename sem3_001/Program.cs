9aa256c4d440b8ea0989bba6d6190145d0c5d12b
Daria <dashavolodina@gmail.com>
2023-02-02 22:47:19 +0800

    полиндром или нет

---
+++ sem3_001/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 1
+// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, 
+// является ли оно палиндромом. Не использовать строки
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число > ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 10000 && num < 99999)
+{
+    int num1 = num / 10000;
+    Console.WriteLine(num1);
+    int num2 = num % 10000 / 1000;
+    Console.WriteLine(num2);
+    int num4 = num % 100 / 10;
+    Console.WriteLine(num4);
+    int num5 = num % 10;
+    Console.WriteLine(num5);
+    if (num1 == num5 && num2 == num4)
+    {
+        Console.WriteLine($"Введенное чило < {num} > является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Введенное чило < {num} > НЕ является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Число должно быть пятизначное");
+}
